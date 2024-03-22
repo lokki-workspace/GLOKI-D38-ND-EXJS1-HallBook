92d358118a509ae0bf903be2b7970d812c2c69b9
i'm LOKKI <lkworkvibes@gmail.com>
2024-03-22 18:13:03 +0530

    hall booking 1st commit

---
+++ data/bookings.js
@@ -0,0 +1,49 @@
+const bookings = [
+    {
+        "bookingId": 1,
+        "customerName": "lokesh",
+        "roomId": 2,
+        "bookedStatus": true,
+        "date": "2024-05-10",
+        "startTime": "10:00",
+        "endTime": "22:00"
+    },
+    {
+        "bookingId": 2,
+        "customerName": "Ram",
+        "roomId": 4,
+        "bookedStatus": true,
+        "date": "2024-04-01",
+        "startTime": "01:00",
+        "endTime": "23:00"
+    },
+    {
+        "bookingId": 3,
+        "customerName": "Kabilan",
+        "roomId": 1,
+        "bookedStatus": true,
+        "date": "2024-04-15",
+        "startTime": "09:30",
+        "endTime": "20:30"
+    },
+    {
+        "bookingId": 4,
+        "customerName": "Sathish",
+        "roomId": 3,
+        "bookedStatus": true,
+        "date": "2024-04-22",
+        "startTime": "11:00",
+        "endTime": "19:00"
+    },
+    {
+        "bookingId": 5,
+        "customerName": "Indhija",
+        "roomId": 5,
+        "bookedStatus": true,
+        "date": "2024-05-30",
+        "startTime": "12:00",
+        "endTime": "14:00"
+    }
+];
+
+module.exports = bookings;
---
+++ data/rooms.js
@@ -0,0 +1,44 @@
+const rooms = [
+    {
+        "roomId": 1,
+        "bookedStatus": false,
+        "roomName": "Bed Room 1",
+        "seatsAvailable": 1,
+        "amenities": ["bed", "pillow"],
+        "price_per_hr": 500
+    },
+    {
+        "roomId": 2,
+        "bookedStatus": false,
+        "roomName": "Restarant",
+        "seatsAvailable": 30,
+        "amenities": ["table","chair"],
+        "price_per_hr": 650
+    },
+    {
+        "roomId": 3,
+        "bookedStatus": false,
+        "roomName": "Bed Room 2",
+        "seatsAvailable": 1,
+        "amenities": ["bed", "pillow"],
+        "price_per_hr": 760
+    },
+    {
+        "roomId": 4,
+        "bookedStatus": false,
+        "roomName": "Bed Room 3",
+        "seatsAvailable": 1,
+        "amenities": ["bed", "pillow"],
+        "price_per_hr": 1000
+    },
+    {
+        "roomId": 5,
+        "bookedStatus": false,
+        "roomName": "Meeting Room",
+        "seatsAvailable": 80,
+        "amenities": ["Mic", "chair"],
+        "price_per_hr": 1500
+    }
+];
+
+module.exports=rooms;
